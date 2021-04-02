9fa5edd47b5cb725685def27d43a14f941603e1b
theunmanifested <waltersoso80@gmail.com>
2021-04-02 18:15:48 -0500

    Complete park_visit table and 1 data insert

--- DB/MyTXStateParksAppdb.sql
+++ DB/MyTXStateParksAppdb.sql
@@ -16,13 +16,23 @@ CREATE SCHEMA IF NOT EXISTS `MyTXStateParksAppdb` DEFAULT CHARACTER SET utf8 ;
 USE `MyTXStateParksAppdb` ;
 
 -- -----------------------------------------------------
--- Table `park`
+-- Table `park_visit`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `park` ;
+DROP TABLE IF EXISTS `park_visit` ;
 
-CREATE TABLE IF NOT EXISTS `park` (
+CREATE TABLE IF NOT EXISTS `park_visit` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NULL,
+  `park_name` VARCHAR(100) NULL,
+  `active` TINYINT NULL DEFAULT 1,
+  `length` TIME NULL,
+  `date` DATETIME NULL,
+  `main_activity` VARCHAR(100) NULL,
+  `log_visit` TEXT NULL,
+  `city` VARCHAR(45) NULL,
+  `county` VARCHAR(45) NULL,
+  `water_activity` TINYINT NULL,
+  `first_time_visit` TINYINT NULL,
+  `img_url` VARCHAR(1000) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -38,11 +48,11 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Data for table `park`
+-- Data for table `park_visit`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `MyTXStateParksAppdb`;
-INSERT INTO `park` (`id`, `name`) VALUES (1, 'Longhorn Cavern State Park');
+INSERT INTO `park_visit` (`id`, `park_name`, `active`, `length`, `date`, `main_activity`, `log_visit`, `city`, `county`, `water_activity`, `first_time_visit`, `img_url`) VALUES (1, 'Longhorn Cavern State Park', 1, NULL, NULL, 'Cavern Walking Tour', 'Cavern walking tour was amazing! Took the whole family spontaneously, and we loved the cave so much. There was a lot of underground geology wonders, history, and bats. ', 'Burnet', 'Burnet', 0, 1, 'https://tpwd.texas.gov/state-parks/longhorn-cavern/gallery/longhorn-caverns_1.jpg');
 
 COMMIT;
 
