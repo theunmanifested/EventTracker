46bfb32f9ea94a4dc9eaeab562f5cf4edaa46b83
theunmanifested <waltersoso80@gmail.com>
2021-04-02 16:05:47 -0500

    Added Spring Boot project

---
+++ BootMyTXStateParksApp/src/main/java/com/skilldistillery/parks/BootMyTxStateParksAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.parks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMyTxStateParksAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMyTxStateParksAppApplication.class, args);
+	}
+
+}
---
+++ BootMyTXStateParksApp/src/test/java/com/skilldistillery/parks/BootMyTxStateParksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.parks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMyTxStateParksAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
