71c58e803162da93793a5fae906ce84c4fba0662
theunmanifested <waltersoso80@gmail.com>
2021-04-10 10:53:49 -0500

    Display a list of all park visits without full details at intial table in landing page

--- BootMyTXStateParksApp/src/main/resources/static/js/script.js
+++ BootMyTXStateParksApp/src/main/resources/static/js/script.js
@@ -1,10 +1,16 @@
-window.addEventListener('load', function(evt) {
+window.addEventListener('load', function (evt) {
     console.log('script.js loaded');
     init();
 });
 
 function init() {
     loadParkVisits();
+    // document.parkVisitsForm.lookup.addEventListener('click', function(event) {
+    //     event.preventDefault();
+    //     var parkVId = document.parkVisitsForm.parkVId.value;
+    //     if (!isNaN(parkVId) && parkVId > 0) {
+    //       getFilm(parkVId);
+    //     }
 }
 
 function loadParkVisits() {
@@ -33,14 +39,44 @@ function displayError(msg) {
 }
 
 function displayParkVisits(parkVisits) {
-    console.log(parkVisits);
-    let div = document.getElementById('parkVisitsTable');
+    let dataDiv = document.getElementById('parkVisitsTable');    
+    dataDiv.textContent = '';
     // TODO: Make a beautiful table
-    // TODO: Add click event for the trs to get parkVisit by Id,
-    // and display details and image etc in parksVisitDisplay
     for (const parkVisit of parkVisits) {
+        
+        let pName = document.createElement('h2');
+        pName.textContent = parkVisit.parkName;
+        dataDiv.appendChild(pName);
+        // let logHeader = document.createElement('h6');
+        // logHeader.textContent = ("Park Visit Log / Details");
+        // dataDiv.appendChild(logHeader);
+        let bQuote = document.createElement('blockquote');
+        bQuote.textContent = "Park Visit Log: "+ parkVisit.logVisit;
+        dataDiv.appendChild(bQuote);
+        let ul = document.createElement('ul');
+        dataDiv.appendChild(ul);
         let li = document.createElement('li');
-        li.textContent = parkVisit.parkName;
-        div.appendChild(li);
+        li.textContent = parkVisit.mainActivity;
+        ul.appendChild(li);
+        li = document.createElement('li');
+        li.textContent = parkVisits.date;
+        ul.appendChild(li);
+        li = document.createElement('li');
+        li.textContent = parkVisits.city;
+        ul.appendChild(li);
+        li = document.createElement('li');
+        li.textContent = parkVisits.county;
+        ul.appendChild(li);
+        li = document.createElement('li');
+        li.textContent = parkVisits.bestSeason;
+        ul.appendChild(li);
+        li = document.createElement('li');
+        li.textContent = parkVisits.length + " minutes";
+        ul.appendChild(li);
     }
+    // TODO: Add click event for the trs to get parkVisit by Id,
+    // and display details and image etc in parksVisitDisplay    
+
+
 }
+
